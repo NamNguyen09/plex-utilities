a5a4dcfc278c6ca7edbbb3fbc73bd33a425f6a4e
NamNguyen09 <nam.nguyenvan@orientsoftware.com>
2023-06-19 15:47:02 +0700

    Initial commit

---
+++ cx.Utiities/IntegerExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+
+namespace cx.Utiities
+{
+    public static class IntegerExtentions
+    {
+        public static bool IsNumericType(this object expression)
+        {
+            if (expression == null || expression is DateTime)
+            {
+                return false;
+            }
+            if (expression is Int16 || expression is Int32 || expression is Int64 || expression is Decimal
+                || expression is Single || expression is Double || expression is Boolean)
+            {
+                return true;
+            }
+
+            double parseVal;
+            var isParse = double.TryParse(expression.ToString(), out parseVal);
+
+            if (isParse)
+                return true;
+
+            isParse = double.TryParse(expression.ToString().Replace(".", ","), out parseVal);
+
+            return isParse;
+        }
+
+        public static bool IsNumericType(this object expression, CultureInfo culture)
+        {
+            if (expression == null || expression is DateTime)
+            {
+                return false;
+            }
+            if (expression is Int16 || expression is Int32 || expression is Int64 || expression is Decimal
+                || expression is Single || expression is Double || expression is Boolean)
+            {
+                return true;
+            }
+            try
+            {
+                if (expression is string)
+                {
+                    double.Parse(expression as string, culture);
+                }
+                else
+                {
+                    double.Parse(expression.ToString(), culture);
+                }
+                return true;
+            }
+            catch // just dismiss errors but return false
+            {
+
+            }
+
+            return false;
+        }
+
+        public static int GetMin(this int number1, int number2, int number3)
+        {
+            return Math.Min(number1, Math.Min(number2, number3));
+        }
+    }
+}
---
+++ cx.Utiities/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cx.Utiities
+{
+    public static class StringExtensions
+    {
+        public static bool EqualsIgnoreCase(this string source, string destination)
+        {
+            if (source == null || destination == null) return false;
+            return source.Equals(destination, StringComparison.OrdinalIgnoreCase);
+        }
+        public static bool ContainsIgnoreCase(this string source, string destination)
+        {
+            if (source == null || destination == null
+                || string.IsNullOrWhiteSpace(source)
+                || string.IsNullOrWhiteSpace(destination)) return false;
+            return source.ToLower().Contains(destination.ToLower());
+        }
+    }
+}
